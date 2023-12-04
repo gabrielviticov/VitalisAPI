009886833d0a36930a0d4819d0d59fbb196a6dd0
Gabriel Viticov <gabrielviticov.ctt@gmail.com>
2023-12-04 16:52:00 -0300

    orm-models: doctor and person properties implemented

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using VitalisAPI.Data;
+using VitalisAPI.Entities.Models;
+using static System.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(
+    opts => opts.UseSqlite(
+        builder.Configuration.GetConnectionString(
+            "DbConnection"
+        )
+    )
+);
+
+var doctor = new DoctorModel();
+{
+    doctor.Person = new PersonModel("Viktor Hugo", "da Silva Santos", "example@example.com", "11 957420313");
+    doctor.Crm = "045321-1";
+    doctor.StartAt = new DateOnly(1999, 01, 04);
+    doctor.EndAt = new DateOnly(2004, 09, 15);
+    WriteLine(doctor.ToString());
+}
 
 var app = builder.Build();
 
